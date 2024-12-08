37e0469010d4f9d7b9e20c72e44bc56eefd28aed
while-basic <c_celaya@icloud.com>
2024-12-07 20:20:39 -0700

    Initial Commit

---
+++ chat-clone/chat_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  chat_cloneApp.swift
+//  chat-clone
+//
+//  Created by Christopher Celaya on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct chat_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ chat-cloneTests/chat_cloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  chat_cloneTests.swift
+//  chat-cloneTests
+//
+//  Created by Christopher Celaya on 12/7/24.
+//
+
+import Testing
+@testable import chat_clone
+
+struct chat_cloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
